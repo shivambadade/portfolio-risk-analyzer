ea07dffef6bd500bfc7705e59f4a306f55ce48fa
Aarya0310 <aaryabhende@gmail.com>
2026-05-25 22:16:57 +0530

    Added portfolio analysis endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,37 @@
 from flask import Flask, jsonify
-from portfolio import get_stock_data
+from portfolio import get_stock_data, analyze_portfolio
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+
     return {
         "message": "Portfolio Risk Analyzer Backend Running"
     }
 
+
 @app.route('/stock/<symbol>')
 def stock(symbol):
+
     data = get_stock_data(symbol.upper())
+
     return jsonify(data)
+
+
+@app.route('/portfolio')
+def portfolio():
+
+    sample_portfolio = {
+        "AAPL": 5,
+        "TSLA": 2,
+        "TCS.NS": 10
+    }
+
+    result = analyze_portfolio(sample_portfolio)
+
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/portfolio.py
+++ backend/portfolio.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 
 def get_stock_data(symbol):
+
     stock = yf.Ticker(symbol)
 
     data = stock.history(period="5d")
@@ -10,4 +11,35 @@ def get_stock_data(symbol):
     return {
         "symbol": symbol,
         "latest_price": round(latest_close, 2)
+    }
+
+
+def analyze_portfolio(portfolio):
+
+    total_value = 0
+
+    stock_details = []
+
+    for symbol, quantity in portfolio.items():
+
+        stock = yf.Ticker(symbol)
+
+        data = stock.history(period="5d")
+
+        latest_price = data['Close'].iloc[-1]
+
+        investment_value = latest_price * quantity
+
+        total_value += investment_value
+
+        stock_details.append({
+            "symbol": symbol,
+            "quantity": quantity,
+            "latest_price": round(latest_price, 2),
+            "investment_value": round(investment_value, 2)
+        })
+
+    return {
+        "total_portfolio_value": round(total_value, 2),
+        "stocks": stock_details
     }
